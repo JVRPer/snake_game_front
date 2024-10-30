f9e34efaa7bf1a02ede45a253e4371b44618d9e4
JVRPer <joaovitorribeiro1110@gmail.com>
2024-10-30 20:13:21 -0300

    Finalizado o trabalho de Front End - Snake

--- game.js
+++ game.js
@@ -1,60 +1,155 @@
-const quadrado = document.getElementById('quadrado');
-const maca = document.getElementById('maca');
-const movimentos = 16;
-let y = 120;
-let x = 0;
-const containerWidth = 500;
-const containerHeigth = 600;
+const areaJogo = document.querySelector('#areaJogo');
+const ctx = areaJogo.getContext('2d')
 
-document.addEventListener('keydown', event => {
+const areaLargura = areaJogo.width;
+const areaAltura = areaJogo.height;
+const tamanho = 20;
+const comida = 'red';
+const cobraCor = 'green';
+let velcidadeX = tamanho;
+let velocidadeY = 0;
+let comidaX;
+let comidaY;
+let direcao
+let cobra = [
+    {x:tamanho * 2, y: 0},
+    {x:tamanho, y: 0},
+    {x:0, y: 0}
+];
+const fundoJogo = 'black';
 
-    if (event.key) {
 
-        switch(event.key) {
-            case 'W':
-            case 'w':
-                if (y >= 130) y -= movimentos;
-                break;
-            case 'S':
-            case 's':
-                if (y < containerHeigth - quadrado.offsetHeight) y += movimentos;
-                break;
-            case 'A':
-            case 'a':
-                if (x > 13) x -= movimentos + 3;
-                break;
-            case 'D':
-            case 'd':
-                if (x < 480) x += movimentos + 0.7;
-                break;
-        }
+setInterval(() => {
+    ctx.fillStyle = fundoJogo;
+    ctx.fillRect(0, 0, areaLargura, areaAltura)
 
-        quadrado.style.top = `${y}px`;
-        quadrado.style.left = `${x}px`;
-        movimentos -= 7;
-        colisao();
+    inicia();
+    corComida();
+    cobraCriada();
+    cobraMovendo();
+    bateuParede();
+    encostouFruta();
+    encostouCauda();
+}, 90);
+
+comidaCriada();
+function inicia(){};
+
+function comidaCriada(){
+    function aleatorioComida (min, max) {
+        const numero = Math.round((Math.random() * (max - min) + min) / tamanho) * tamanho;
+        return numero;
     }
-});
+    comidaX = aleatorioComida(0, areaLargura - tamanho);
+    comidaY = aleatorioComida(0, areaLargura - tamanho);
+    console.log(comidaX);
+};
 
-function colisao() {
+function corComida(){
+    ctx.fillStyle = comida;
+    ctx.fillRect(comidaX, comidaY, tamanho, tamanho);
+};
+function cobraCriada(){
+    ctx.fillStyle = cobraCor;
+    cobra.forEach(cobraPartes => {
+        ctx.fillRect(cobraPartes.x, cobraPartes.y, tamanho, tamanho)
+    })
+};
 
-    const quadradoArea = quadrado.getBoundingClientRect();
-    const macaArea = maca.getBoundingClientRect();
+function cobraMovendo(){
+    if(!direcao) return;
+    const cabeca = { ...cobra[cobra.length - 1] }
 
-    const colisao = !(quadradoArea.right < macaArea.left ||
-                      quadradoArea.left > macaArea.right ||
-                      quadradoArea.bottom < macaArea.top ||
-                      quadradoArea.top > macaArea.bottom);
-    if (colisao) {
-        quadrado.style.width = `${quadradoArea.width + 5}px`;
+    if(direcao == 'direita') {
+        cabeca.x += tamanho;
+    }
 
-        const maxtop =  containerHeigth - maca.offsetHeight;
-        const maxleft = containerWidth - maca.offsetWidth;
-        
-        const posicaoAleatoriaLeft = Math.random() * maxleft;
-        const posicaoAleatoriaTop = Math.random() * maxtop;
+    if(direcao == 'esquerda') {
+        cabeca.x -= tamanho;
+    }
 
-        maca.style.left = `${Math.round(posicaoAleatoriaLeft)}px`;
-        maca.style.top = `${posicaoAleatoriaTop}px`;
+    if(direcao == 'baixo') {
+        cabeca.y += tamanho;
+    }
+
+    if(direcao == 'cima') {
+        cabeca.y -= tamanho;
+    }
+
+    cobra.push(cabeca)
+
+    if(!encostouFruta()) {
+        cobra.shift()
+    }
+};
+
+document.addEventListener('keydown', ({key}) =>{
+    console.log(event.key)
+
+    if(key == 'W' && direcao != 'baixo') {
+        direcao = 'cima'
+    }
+
+    if(key == 'w' && direcao != 'baixo') {
+        direcao = 'cima'
+    }
+
+    if(key == 'S' && direcao != 'cima') {
+        direcao = 'baixo'
+    }
+
+    if(key == 's' && direcao != 'cima') {
+        direcao = 'baixo'
+    }
+
+    if(key == 'D' && direcao != 'esquerda') {
+        direcao = 'direita'
+    }
+
+    if(key == 'd' && direcao != 'esquerda') {
+        direcao = 'direita'
+    }
+
+    if(key == 'A' && direcao != 'direita') {
+        direcao = 'esquerda'
+    }
+
+    if(key == 'a' && direcao != 'direita') {
+        direcao = 'esquerda'
+    }
+})
+
+function bateuParede(){
+    const cabeca = cobra[0]
+    if(cabeca.x < 0 || cabeca.x >= areaAltura || cabeca.y < 0 || cabeca.y > areaLargura) {
+        cabeca.x = 0;
+        location.reload()
+        return true;
     }
 }
+
+
+function encostouFruta(){
+    const cabeca = cobra[cobra.length - 1];
+    if(cabeca.x == comidaX && cabeca.y == comidaY) {
+        comidaCriada();
+        cobra.push(cabeca)
+        ctx.fillStyle = cobraCor;
+    cobra.forEach(cobraPartes => {
+        ctx.fillRect(cobraPartes.x, cobraPartes.y, tamanho, tamanho)
+        return true
+    })
+}}
+
+function encostouCauda(){
+    const cabeca = cobra[cobra.length - 1];
+    const bateu = cobra.slice(1, -2).some(position =>
+        position.x === cabeca.x && position.y === cabeca.y
+);
+    
+    if(bateu) {
+        console.log('bateu')
+        location.reload()
+    }
+}
+
